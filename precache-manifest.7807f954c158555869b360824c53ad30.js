721b822a0a0fb3641fecddecec3f110a497e9f12
Gustavo Veiga <gustavomorenodaveiga@live.com>
2019-11-13 12:39:40 -0300

    Updates

---
+++ precache-manifest.7807f954c158555869b360824c53ad30.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0810453255fedc1c106cd0b1c50e50",
+    "url": "/ihc-tesla/index.html"
+  },
+  {
+    "revision": "689e71fd22715a9aa906",
+    "url": "/ihc-tesla/static/js/2.de217607.chunk.js"
+  },
+  {
+    "revision": "cbee7e54ca700a77322d",
+    "url": "/ihc-tesla/static/js/main.c7a0784c.chunk.js"
+  },
+  {
+    "revision": "304070a3947f9bb409d3",
+    "url": "/ihc-tesla/static/js/runtime-main.ea318a6e.js"
+  }
+]);
