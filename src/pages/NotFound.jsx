43f1825d2036e171bde421c9fbb174f4a2803bf2
Gustavo Veiga <gustavomorenodaveiga@live.com>
2019-11-21 10:41:33 -0300

    Page Not Found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,29 @@
-import React, { Component }  from "react";
+import React from "react";
+import Lottie from "react-lottie";
+import { Helmet } from "react-helmet";
+import { useTranslation } from "react-i18next";
+import animationData from "../animations/page-not-found.json"
 
-export default class NotFoundPage extends Component {
-  render () {
-    return (
-      <div>
-        <h1>404 - Not Found</h1>
-      </div>
-    )
-  }
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  return (
+    <div>
+      <Helmet>
+        <title>{t("Not Found")}</title>
+      </Helmet>
+      <Lottie options={defaultOptions} height={450} width={450} />
+    </div>
+  )
 }
+
+export default NotFoundPage;
